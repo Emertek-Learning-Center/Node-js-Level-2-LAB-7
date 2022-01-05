fe9a877e87fae37f9f6095fccd858de374a4acc0
DevOps-T04 <91058125+DevOps-T04@users.noreply.github.com>
2022-01-05 22:06:22 +0100

    proxy-server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 const axios = require("axios")
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
 
-
-axios.get('https://reqres.in/api/users')
-.then(res => {
-    console.log(res.data);
+app.get('/', (req, res) => {
+    res.json('go to /weather/COUNTRY_NAME to get weather data')
 })
+
+// requesting weather data for a specific country
+// Country name is being passed as a parameter and not in the query string
+app.get('/weather/:country', (req, res)=>{
+
+    let countryName = req.params.country
+    let apiKey = "API_KEY_HERE"
+    
+    // Constructing the final URL
+    let URL = `api.openweathermap.org/data/2.5/weather?q=${countryName}&appid=${apiKey}`
+    console.log("URL :::::: " + URL);
+    // Using Axios to retrieve weather date
+    axios.get(URL)
+    .then(res => {
+        console.log(res.data);
+    })
+
+})
+
+// Making sure all other requests are treated as 404
+app.get('*', (req, res)=>{
+    res.json('404 ! Nothing here 🙈')
+})
+app.listen(PORT, console.log(`🔥 Live on port ${PORT}`))
